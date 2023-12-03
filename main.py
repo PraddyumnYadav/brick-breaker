46967ba05dc24f340c6371dc478db71eff0c7b4e
Praddyumn Yadav <praddyumny@gmail.com>
2023-12-03 20:43:42 +0530

    Draw Basic Shield and Ball.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# ---------------------------------Creating Classic Brick Breaker Game with Python---------------------------------
+# --------------------------Creating Classic Brick Breaker Game with Python-------------------------- #
 import pygame
 
 # Create Global Variables
@@ -6,14 +6,37 @@ WIDTH, HEIGHT = 800, 675
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Brick Breaker - Praddyumn Yadav")
 
+SHIELD_WIDTH = 250
+SHIELD_HEIGHT = 25
+
+BALL_WIDTH = 25
+
+
+def draw(shield, ball_x, ball_y):
+	pygame.draw.circle(WIN, "red", (ball_x, ball_y), BALL_WIDTH)
+	pygame.draw.rect(WIN, "white", shield)
+	pygame.display.update()
+
 
 def main():
 	run = True
+
+	shield = pygame.Rect(
+		int(WIDTH / 2 - SHIELD_WIDTH / 2),
+		int(HEIGHT - SHIELD_HEIGHT - 50),
+		SHIELD_WIDTH,
+		SHIELD_HEIGHT
+	)
+
+	ball_x = int(WIDTH/2)
+	ball_y = int(HEIGHT - SHIELD_HEIGHT - 50 - BALL_WIDTH)
 
 	while run:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
+
+		draw(shield, ball_x, ball_y)
 
 
 if __name__ == "__main__":
