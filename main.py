2d5b70a3d9e6e2363c92351979a37f15f9fae9f0
Praddyumn Yadav <praddyumny@gmail.com>
2023-12-03 21:54:16 +0530

    Draw Bricks.

--- main.py
+++ main.py
@@ -11,66 +11,91 @@ SHIELD_HEIGHT = 25
 
 BALL_WIDTH = 25
 
+BRICK_WIDTH = 50
+BRICK_HEIGHT = 25
 
-def draw(shield, ball_x, ball_y):
-	# Clear the Screen
-	WIN.fill((0, 0, 0))
+BRICKS_PER_ROW = 10
 
-	# Draw The Ball and Shield
-	pygame.draw.circle(WIN, "red", (ball_x, ball_y), BALL_WIDTH)
-	pygame.draw.rect(WIN, "white", shield)
 
-	# Update the Display
-	pygame.display.update()
+def draw(shield, ball_x, ball_y, bricks):
+    # Clear the Screen
+    WIN.fill((0, 0, 0))
+
+    # Draw The Ball and Shield
+    pygame.draw.circle(WIN, "red", (ball_x, ball_y), BALL_WIDTH)
+    pygame.draw.rect(WIN, "white", shield)
+
+    # Draw Bricks
+    for brick in bricks:
+        pygame.draw.rect(WIN, "blue", brick)
+
+    # Update the Display
+    pygame.display.update()
 
 
 def main():
-	run = True
+    run = True
 
-	shield = pygame.Rect(
-		int(WIDTH / 2 - SHIELD_WIDTH / 2),
-		int(HEIGHT - SHIELD_HEIGHT - 50),
-		SHIELD_WIDTH,
-		SHIELD_HEIGHT
-	)
+    shield = pygame.Rect(
+        int(WIDTH / 2 - SHIELD_WIDTH / 2),
+        int(HEIGHT - SHIELD_HEIGHT - 50),
+        SHIELD_WIDTH,
+        SHIELD_HEIGHT,
+    )
 
-	ball_x = int(WIDTH/2)
-	ball_y = int(HEIGHT - SHIELD_HEIGHT - 50 - BALL_WIDTH)
+    ball_x = int(WIDTH / 2)
+    ball_y = int(HEIGHT - SHIELD_HEIGHT - 50 - BALL_WIDTH)
 
-	ball_vel_x = 5
-	ball_vel_y = 5
+    ball_vel_x = 5
+    ball_vel_y = 5
 
-	clock = pygame.time.Clock()
+    bricks = []
+    level = 0
 
-	while run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
+    clock = pygame.time.Clock()
 
-		draw(shield, ball_x, ball_y)
+    while run:
+        if len(bricks) == 0:
+            level += 1
+            x = int(WIDTH/2 - BRICK_WIDTH*5 - 10*5)
+            y = 50
+            for _ in range(level):
+                for _ in range(BRICKS_PER_ROW):
+                    brick = pygame.Rect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
+                    x += BRICK_WIDTH + 10
+                    bricks.append(brick)
+                # Move to the next row
+                y += BRICK_HEIGHT + 10
+                x = 50  # Reset x position for the next row
 
-		# Check Collision from Walls
-		if ball_x+BALL_WIDTH >= WIDTH or ball_x-BALL_WIDTH <= 0:
-			ball_vel_x = -ball_vel_x
-		if ball_y+BALL_WIDTH >= HEIGHT or ball_y-BALL_WIDTH <= 0:
-			ball_vel_y = -ball_vel_y
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-		# Check Collision from Shield
-		if shield.colliderect(
-			pygame.Rect(
-				ball_x - BALL_WIDTH,
-				ball_y - BALL_WIDTH,
-				2 * BALL_WIDTH,
-				2 * BALL_WIDTH,
-			)
-		):
-			ball_vel_y = -ball_vel_y
+        # Check Collision from Walls
+        if ball_x + BALL_WIDTH >= WIDTH or ball_x - BALL_WIDTH <= 0:
+            ball_vel_x = -ball_vel_x
+        if ball_y + BALL_WIDTH >= HEIGHT or ball_y - BALL_WIDTH <= 0:
+            ball_vel_y = -ball_vel_y
 
-		ball_x += ball_vel_x
-		ball_y += ball_vel_y
+        # Check Collision from Shield
+        if shield.colliderect(
+            pygame.Rect(
+                ball_x - BALL_WIDTH,
+                ball_y - BALL_WIDTH,
+                2 * BALL_WIDTH,
+                2 * BALL_WIDTH,
+            )
+        ):
+            ball_vel_y = -ball_vel_y
 
-		clock.tick(60)
+        ball_x += ball_vel_x
+        ball_y += ball_vel_y
+
+        draw(shield, ball_x, ball_y, bricks)
+
+        clock.tick(60)
 
 
 if __name__ == "__main__":
-	main()
+    main()
